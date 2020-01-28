6926f8f5c475f33d31b15a2515c78cf980a9ea0f
Karlo Paragas <karloparagas@gmail.com>
2020-01-28 09:29:08 -0800

    Added clothes controller

---
+++ TinyClothesMVC/Controllers/ClothesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TinyClothesMVC.Data;
+
+namespace TinyClothesMVC.Controllers
+{
+    public class ClothesController : Controller
+    {
+        private readonly StoreContext _context; //readonly means that only the constructor can modify this variable
+
+        public ClothesController(StoreContext context) 
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult ShowAll()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add() 
+        {
+            return View();
+        }
+    }
+}
