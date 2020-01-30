86745b9d723887ee9eb692a00e268c0c69fc9253
Karlo Paragas <karloparagas@gmail.com>
2020-01-30 10:14:01 -0800

    Added method to get inventory by page

--- TinyClothesMVC/Data/ClothingDb.cs
+++ TinyClothesMVC/Data/ClothingDb.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,9 +12,34 @@ namespace TinyClothesMVC.Data
     /// </summary>
     public static class ClothingDb
     {
-        public static List<Clothing> GetAllClothing() 
+        //If you wanted page 1, we woouldn't skip ay rows, so we must offset by 1
+        const int PAGE_OFFSET = 1;
+
+        /// <summary>
+        /// Returns a specific page of clothing items sorted by ItewmId in ascending order
+        /// </summary>
+        /// <param name="context">The DB context passed from the clothes controller</param>
+        /// <param name="pageNum">The page</param>
+        /// <param name="pageSize">Number of Clothing per page</param>
+        public static async Task<List<Clothing>> GetClothingByPage(StoreContext context, int pageNum, int pageSize) //StoreContext context gets passed 
+                                                                                                                    //from the clothes controller
         {
-            throw new NotImplementedException();
+            //LINQ Method Syntax
+            List<Clothing> clothes = await context.Clothing
+                                                  .OrderBy(c => c.ItemId)
+                                                  .Skip((pageNum - PAGE_OFFSET) * pageSize)
+                                                  .Take(pageSize)
+                                                  .ToListAsync(); //Note: Order matters on this
+
+            //LINQ Query Syntax
+            //List<Clothing> clothes2 = await (from c in context.Clothing
+            //                                 orderby c.ItemId ascending
+            //                                 select c)
+            //                                .Skip((pageNum - PAGE_OFFSET) * pageSize)
+            //                                .Take(pageSize)
+            //                                .ToListAsync(); //Note: Order matters on this
+
+            return clothes;
         }
 
         /// <summary>
