9690bae21b7d0121cecfba50498b2dad75d4e9d0
Karlo Paragas <karloparagas@gmail.com>
2020-02-25 10:37:54 -0800

    Added cart controller

---
+++ TinyClothesMVC/Controllers/CartController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TinyClothesMVC.Data;
+using TinyClothesMVC.Models;
+
+namespace TinyClothesMVC.Controllers
+{
+    public class CartController : Controller
+    {
+        //To read cookie data
+        private readonly StoreContext _context;
+        private readonly IHttpContextAccessor _http;
+
+        public CartController(StoreContext context, IHttpContextAccessor http) 
+        {
+            _context = context;
+            _http = http;
+        }
+
+        //Display all products in cart
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Add a single product to the shopping cart
+        public async Task<IActionResult> Add(int id, string prevUrl) 
+        {
+            Clothing c = await ClothingDb.GetClothingById(id, _context);
+
+            if (c != null) 
+            {
+                CartHelper.Add(c, _http);
+            }
+            return Redirect(prevUrl);
+        }
+
+        //Summary/Checkout page
+        public IActionResult Checkout() 
+        {
+            return View();
+        }
+    }
+}
